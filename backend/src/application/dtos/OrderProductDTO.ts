35df61d9d6d8980f8995b241721d73fdf8d99723
bruno-oli <brunomaz177@gmail.com>
2023-11-26 16:39:14 -0300

    add OrderProductRepository

---
+++ backend/src/application/dtos/OrderProductDTO.ts
@@ -0,0 +1,6 @@
+export interface OrderProductDTO {
+  order_id: string
+  product_id: string
+  quantity: number
+  price: number
+}
---
+++ backend/src/domain/repositories/OrderProductRepository.ts
@@ -0,0 +1,9 @@
+import { OrderProductDTO } from "@/application/dtos/OrderProductDTO";
+import { OrderProduct } from "../entities/OrderProduct";
+
+export interface OrderProductRepository {
+  create(orderProduct: OrderProductDTO): Promise<void>;
+  findById(id: string): Promise<OrderProduct>;
+  update(id: string, orderProduct: Partial<OrderProductDTO>): Promise<void>;
+  delete(id: string): Promise<void>;
+}
