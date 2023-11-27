79fd43b0fbd65bde499c7cf2af07783b8b416517
bruno-oli <brunomaz177@gmail.com>
2023-11-26 23:56:14 -0300

    verify user id in authusermiddleware

---
+++ backend/src/interfaces/middlewares/AuthUserMiddleware.ts
@@ -0,0 +1,38 @@
+import { AuthUserDTO } from '@/application/dtos/UserDTO'
+import { UserRepository } from '@/domain/repositories/UserRepository'
+import { NextFunction, Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+
+class AuthUserMiddleware {
+  private readonly userRepository: UserRepository
+
+  constructor(userRepository: UserRepository) {
+    this.userRepository = userRepository
+  }
+
+  async handle(req: Request, res: Response, next: NextFunction) {
+    const token = req.headers.authorization
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' })
+    }
+
+    const secret = process.env.JWT_SECRET
+
+    try {
+      const decoded = jwt.verify(token, secret) as AuthUserDTO
+
+      const user = await this.userRepository.findById(decoded.id)
+
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' })
+      }
+
+      return next()
+    } catch (error) {
+      return res.status(401).json({ error: 'Unauthorized' })
+    }
+  }
+}
+
+export { AuthUserMiddleware }
